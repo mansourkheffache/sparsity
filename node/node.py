0b00e50ef3021184de7bf8264c6cdef16a44d900
Mansour Kheffache <mansourkheffache@gmail.com>
2018-10-09 23:26:40 +0200

    actually, turned it into a list for consistency, might revert back later

--- node/node.py
+++ node/node.py
@@ -91,7 +91,7 @@ class Node:
 		index = int(np.log2(node_id ^ origin_id))
 
 		# HACK filters out all None elements - no neighbors at that distance
-		return filter(None, self.neighbors[:index])
+		return list(filter(None, self.neighbors[:index]))
 
 
 	def join(self):
