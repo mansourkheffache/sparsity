1635cbe9585004518b6ae65089d78210863000db
Mansour Kheffache <mansourkheffache@gmail.com>
2018-10-09 22:10:39 +0200

    added proper communication formats for store and retrieve

--- node/node.py
+++ node/node.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 
 import numpy as np
 import xmlrpc.client
@@ -6,6 +8,7 @@ from xmlrpc.server import SimpleXMLRPCServer
 
 from threading import Thread
 import socket
+import uuid
 
 # TODO side stuff to move to utils.py
 # just threads, but join() has a return value -- useful for this use case
@@ -22,6 +25,7 @@ class BThread(Thread):
     def join(self):
         Thread.join(self)
         return self._return
+
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # TODO change 8.8.8.8 with tracker ??
@@ -38,30 +42,37 @@ class Node:
 		# C:	bounds
 
 		self.X = X
-		self.bins = np.zeros((N, X))
+		self.bins = np.zeros((N, X), dtype=int)
 		self.threshold = T
 		self.addresses = np.random.randint(2, size=(N, X))
 		self.bounds = C
 		self.tracker = tracker
 
+		# tmp fix without tracker
+		self.neighbors = []
+		self.id = uuid.uuid4().hex[:5]
+
 
 	def _set(self, data):
 		# get matching locations
-		distances = np.count_nonzero(data != self.addresses, axis=1)
-		locations = np.where(distances <= self.threshold)
+		distances = np.count_nonzero(np.array(data, dtype=int) != self.addresses, axis=1)
+		locations = np.where(distances <= self.threshold)[0]
 
 		# store data into designated locations	
-		bipolar_data = data * 2 - 1
+		bipolar_data = np.array(data, dtype=int) * 2 - 1
 		self.bins[locations] += bipolar_data
 		self.bins[self.bins < -self.bounds] = -self.bounds
 		self.bins[self.bins > self.bounds] = self.bounds
+
 
 		return len(locations)
 
 
 	def _get(self, address):
+
+
 		# get matching locations
-		distances = np.count_nonzero(address != self.addresses, axis=1)
+		distances = np.count_nonzero(np.array(address, dtype=int) != self.addresses, axis=1)
 		locations = np.where(distances <= self.threshold)
 
 		# get sum of bins
@@ -70,6 +81,7 @@ class Node:
 		return v
 
 	def _get_recipients(self, origin):
+
 		# turn all to numpy world
 		# neighbors_ids = np.array([ int(n['id'], 16) for n in self.neighbors ], dtype=int) 		# needed??
 		node_id = int(self.id, 16)
@@ -94,12 +106,12 @@ class Node:
 		count = 0
 
 		# local
-		t = BThread(target=self._set, args=(data))
+		t = BThread(target=self._set, args=(data,))
 		threads.append(t)
 		t.start()
 
 		# remote
-		for r in _get_recipients(origin):
+		for r in self._get_recipients(origin):
 			with xmlrpc.client.ServerProxy('http://' + r['ip'] + ':' + r['port'] + '/') as proxy:
 				t = BThread(target=proxy.store, args=(data, self.id))
 				threads.append(t)
@@ -116,35 +128,45 @@ class Node:
 	def retrieve(self, address, origin):
 
 		threads = []
-		v = np.zeros(self.X)
+		v = np.zeros(self.X, dtype=int)
 
 		# local
-		t = BThread(target=self._get, args=(address))
+		t = BThread(target=self._get, args=(address,))
 		threads.append(t)
 		t.start()
 
 		# remote
-		for r in _get_recipients(origin):
+		for r in self._get_recipients(origin):
 			with xmlrpc.client.ServerProxy('http://' + r['ip'] + ':' + r['port'] + '/') as proxy:
 				t = BThread(target=proxy.retrieve, args=(address, self.id))
 				threads.append(t)
 				t.start()
 
+
+
 		# TODO do you need string encode/decode??
+		# Nej
+
 		# wait
 		for t in threads:
 			v += t.join()
 
-		return v
+		return v.tolist()
+
+	def print_info(self):
+		print(self.addresses)
+		print(self.bins)
 
 
 
-# start XML-RPC server
-# with SimpleXMLRPCServer(('localhost', 8000)) as server:
-# 	server.register_instance(Node(20, 10, 8, 5, 'lul'))
-# 	print('Node running on localhost port 8000')
-# 	try:
-# 		server.serve_forever()
-# 	except KeyboardInterrupt:
-# 		print("\nKeyboard interrupt received, exiting.")
-# 		sys.exit(0)
+if __name__ == "__main__":
+
+	# start XML-RPC server
+	with SimpleXMLRPCServer(('localhost', 8000), use_builtin_types=True) as server:
+		server.register_instance(Node(20, 10, 8, 5, 'lul'))
+		print('Node running on localhost port 8000')
+		try:
+			server.serve_forever()
+		except KeyboardInterrupt:
+			print("\nKeyboard interrupt received, exiting.")
+			sys.exit(0)
